1ce3e58da9200f3951dfa6bf9883924ef251917d
yury.kaliada <fut.wrk@gmail.com>
2013-05-27 14:22:03 +0300

    Initial commit
    Signed-off-by: yury.kaliada <fut.wrk@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XStore::Application.config.secret_token = '4a7edb581d6d684a9adfdbb4f118b34d091a262e03eaaed2b85717d4a76e92ef4abe5877776a4ce56444255f24a0b5b67c769545b0b1f5401c50082847268147'
