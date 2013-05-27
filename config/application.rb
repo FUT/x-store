3064b10e79ef881dc4d422879bd79530bd1b2f7d
yury.kaliada <fut.wrk@gmail.com>
2013-05-27 14:47:34 +0300

    Fix heroku assets precompilation bug
    Signed-off-by: yury.kaliada <fut.wrk@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module XStore
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Fix heroku assets precompilation bug
+    config.assets.initialize_on_precompile = false
   end
 end
